8cda3a63e8b93385eee0949df59aa74e33bbff46
Hafiya Siddiqha <hafiyasoftwaredeveloper1@gmail.com>
2026-04-06 19:37:28 +0530

    feat: create a vertical bar chart for categories
    Implemented a bar chart to compare discrete categories, such as sales performance or population across different cities.

---
+++ 01_NumPy_Basics/34_Matplotlib_Bar_Chart.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+
+cities = ['Pedana', 'Hyderabad', 'Bangalore']
+salary = [28800, 45000, 55000]
+
+plt.bar(cities, salary, color='skyblue')
+plt.title("Salary Comparison")
+plt.show()
